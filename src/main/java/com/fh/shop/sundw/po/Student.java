f8b6b4fce34f172ce1d7c4d10ea1f6a9197727e9
sundw <18435731863@163.com>
2019-08-03 10:38:02 +0800

    dawei

---
+++ src/main/java/com/fh/shop/sundw/po/Student.java
@@ -0,0 +1,12 @@
+package com.fh.shop.sundw.po;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class Student implements Serializable {
+
+    private String stuName;
+
+    private int age;
+}
